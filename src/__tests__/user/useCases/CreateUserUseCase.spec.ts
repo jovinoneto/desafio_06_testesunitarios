20537154828506d3bd9acde76ecdd47fc5457c69
Jovino Neto <jovinone@gmail.com>
2021-07-15 11:51:23 -0300

    challenge finished

---
+++ src/__tests__/user/useCases/CreateUserUseCase.spec.ts
@@ -0,0 +1,41 @@
+import { InMemoryUsersRepository } from "../../../modules/users/repositories/in-memory/InMemoryUsersRepository";
+import { CreateUserError } from "../../../modules/users/useCases/createUser/CreateUserError";
+import { CreateUserUseCase } from "../../../modules/users/useCases/createUser/CreateUserUseCase";
+
+let createUserUseCase: CreateUserUseCase;
+let inMemoryUsersRepository: InMemoryUsersRepository;
+
+describe("Create User", () => {
+  beforeEach(() => {
+    jest.setTimeout(8000);
+    inMemoryUsersRepository = new InMemoryUsersRepository();
+    createUserUseCase = new CreateUserUseCase(inMemoryUsersRepository);
+  });
+
+  afterAll(() => {
+    jest.clearAllTimers();
+  });
+
+  it("should be able to create a new user", async () => {
+    const user = await createUserUseCase.execute({
+      name: "userCreate",
+      email: "usercreate@email.com.br",
+      password: "password",
+    });
+    
+    expect(user).toHaveProperty("id");
+  });
+
+  it("should not be able to create a new user with already used email", async () => {
+    expect(async () => {
+      const user = await createUserUseCase.execute({
+        name: "userAlreadyExists",
+        email: "useralreadyexists@email.com.br",
+        password: "password"
+      });
+  
+      await createUserUseCase.execute(user);
+    }).rejects.toBeInstanceOf(CreateUserError);
+  });
+
+});
